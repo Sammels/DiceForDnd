8652685e1b401ae1d3129139d49ae409ddb6d969
Sammels <Sasagete@gmail.com>
2018-01-17 19:31:27 +0300

    Just Dice
    Dica from D4 at D100

---
+++ Dice.py
@@ -0,0 +1,29 @@
+import random as rd
+
+# Make 5 dice
+
+
+def D4():
+    D4 = rd.randrange(1, 5)
+    print(D4)
+
+def D6():
+    D6 = rd.randrange(1, 7)
+    print(D6)
+
+def D8():
+    D8 = rd.randrange(1, 9)
+    print (D8)
+
+def D10():
+    D10 = rd.randrange(1, 11)
+    print(D10)
+
+def D12():
+    D12 = rd.randrange(1, 13)
+    print(D12)
+
+def D20():
+    D20 = rd.randrange(1, 21)
+    print(D20)
+    
